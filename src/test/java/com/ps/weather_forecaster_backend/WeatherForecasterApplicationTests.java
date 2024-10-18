3950ac4e9c0b5266fd19ce18aabfd5eb6d8331e0
prabalsri <prabal.srivastava@hyland.com>
2024-10-18 20:11:37 +0530

    Bootstraping spring boot project with initializer

---
+++ src/test/java/com/ps/weather_forecaster_backend/WeatherForecasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.weather_forecaster_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
