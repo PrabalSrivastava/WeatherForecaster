60da7d0cf1820152da5a8d4d9801464bd934301d
PrabalSrivastava <prabalsrivastava99@gmail.com>
2024-10-21 15:36:11 +0530

    Fixing some bugs

--- frontend/src/app/weather-display/weather-display.component.ts
+++ frontend/src/app/weather-display/weather-display.component.ts
@@ -28,6 +28,13 @@ export class WeatherDisplayComponent implements OnInit {
     });
   }
 
+  capitalizeCity(city: string): string {
+    if (!city) return city;
+    return city.split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+  }
+
   fetchWeather() {
     if (this.city) {
       this.weatherService.getWeather(this.city).pipe(
--- src/main/java/com/ps/weather_forecaster_backend/client/entity/OpenWeatherData.java
+++ src/main/java/com/ps/weather_forecaster_backend/client/entity/OpenWeatherData.java
@@ -1,5 +1,6 @@
 package com.ps.weather_forecaster_backend.client.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import java.util.List;
 
 @Getter @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OpenWeatherData {
 
     private long dt;
